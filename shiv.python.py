0b9d295681ab2051ed35d9b4cae77ae267b3cf1a
preet7020 <159771552+preet7020@users.noreply.github.com>
2024-03-18 15:13:02 +0530

    Add files via upload

---
+++ shiv.python.py
@@ -0,0 +1,9 @@
+x=10
+y=5.5
+name="John"
+is_student=True
+print("Value of x:",x,"Type:",type(x))
+print("Value of y:",y,"Type:",type(y))
+print("value of name:",name,"Type:",type(name))
+print("Value of is_student:",is_student,"Type:",type(is_student))
+                                  
